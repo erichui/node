52ba74141ca7a53c78654ebf8cb1dd2cdaa699ee
erichui <erichui0906@163.com>
2017-09-26 18:22:42 +0800

    zlib

---
+++ zlib/index.js
@@ -0,0 +1,17 @@
+let fs = require('fs')
+let zlib = require('zlib')
+
+let gzip = zlib.createGzip()
+let gunzip = zlib.createGunzip()
+let readS = fs.createReadStream('./source.txt')
+let writeS = fs.createWriteStream('./destination.txt.gz')
+
+readS.pipe(gzip).pipe(writeS)
+// readS.pipe(gunzip).pipe(writeS)
+
+// readS.on('end', () => {
+//   console.log('done')
+// })
+// gunzip.on('error', (err) => {
+//   console.log('err', err)
+// })
