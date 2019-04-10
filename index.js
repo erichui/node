63955c0a9b6b7381c376cfecf9ea96c25424cca3
erichui <erichui0906@163.com>
2019-04-10 19:38:31 +0800

    file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const fs = require('fs')
 
-fs.readdir(__dirname, (err, files) => {
+fs.readdir(process.cwd(), (err, files) => {
   if(err) {
-    console.log(err)
-  } else {
-    console.log(files)
+    return console.log(err)
   }
+  console.log('')
+  if(!files.length) {
+    return console.log('no files')
+  }
+  console.log('select which file or directory you want to see\n')
 })
